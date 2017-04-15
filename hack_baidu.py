c13ff8bcfab7fbc2f5b5355d05b392ff1ffb336c
NaiveWang <wanhchiwen@yahoo.com>
2017-04-15 23:09:38 +0800

    Debugged

--- hack_baidu.py
+++ hack_baidu.py
@@ -32,20 +32,21 @@ while 1:
     s = bytes.decode(html)
     #print(s.__len__())
     if(s.find(warn)==-1):
-        print("此吧健在\n")
-        if s.find('关于撤销'):
-            if s.find('吧主权限'):
-                print("+++此吧吧主被撤销")
+        print("此吧健在。")
+        if s.find('关于撤销')!=-1:
+            if s.find('吧主权限')!=-1:
+                print("+++监测到此吧吧主被撤销")
         if s.find('苟')!=-1:
-            print("+++有人在念诗")
+            print("+++监测到有人在念诗")
         if s.find('吼啊')!=-1:
-            print("+++有人在大吼")
+            print("+++监测到有人在大吼")
         if s.find('蛤')!=-1:
-            print("+++有人在召唤长者")
+            print("+++监测到有人在召唤长者")
         if s.find('naive')!=-1:
-            print("+++有人在得罪一下")
+            print("+++监测到有人在得罪一下")
         if s.find('给我搞')!=-1:
-            print("+++有人正在赛艇")
+            print("+++监测到有人正在赛艇")
+        print("")
     else:
         print("此吧已经被续\n")
         c.execute("INSERT INTO blocked VALUES(\'"+parse.unquote(name)+"\',CURRENT_TIMESTAMP)")
