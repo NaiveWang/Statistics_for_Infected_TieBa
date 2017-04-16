fb20ac1dc76f6d8025a3869069f87cf7901e74c5
NaiveWang <wanhchiwen@yahoo.com>
2017-04-16 11:41:51 +0800

    Optimizing
    The output line

--- hack_baidu.py
+++ hack_baidu.py
@@ -11,12 +11,12 @@ while 1:
     name = input()
     c.execute("SELECT * FROM blocked WHERE name=\'"+name+"\'")
     if c.fetchone()!=None:
-        print("数据库显示此吧已经被续")
+        print("#---数据库显示此吧已经被续")
         continue
     c.execute("SELECT *FROM infected WHERE name=\'"+name+"\'")
     if c.fetchone()!=None:
         for row in c.execute("SELECT *FROM infected WHERE name=\'"+name+"\'"):
-            print("数据库显示此吧已列入感染名单，匹配度为："+row[1].__str__())
+            print("#---数据库显示此吧已列入感染名单，匹配度为："+row[1].__str__())
         continue
     name = parse.quote(name)
     url0 = url0+name
@@ -26,7 +26,7 @@ while 1:
     #print(response.code)
 
 
-    print("数据库空，正在抓取此吧")
+    print("O--数据库空，正在抓取此吧")
 
     html = response.read()
     #print(len(html))
@@ -35,41 +35,41 @@ while 1:
     try:
         s = bytes.decode(html)
     except Exception as e:
-        print("暂无此吧")
+        print("X---暂无此吧")
         continue
 
     #print(s.__len__())
     if(s.find(warn)==-1):
-        print("此吧健在。")
+        print("O---此吧健在。")
         ct = 0
         if s.find('关于撤销')!=-1:
             if s.find('吧主权限')!=-1:
-                print("+++监测到此吧吧主被撤销")
+                print("++++++监测到此吧吧主被撤销")
                 ct+=1
         if s.find('苟')!=-1:
-            print("+++监测到有人在念诗")
+            print("++++监测到有人在念诗")
             ct+=5
             if s.find('利')!=-1:
                 ct+=45
         if s.find('吼啊')!=-1:
-            print("+++监测到有人在大吼")
+            print("++++监测到有人在大吼")
             ct+=50
         if s.find('蛤')!=-1:
-            print("+++监测到有人在召唤长者")
+            print("++++监测到有人在召唤长者")
             ct+=10
         if s.find('naive')!=-1:
-            print("+++监测到有人在得罪一下")
+            print("++++监测到有人在得罪一下")
             ct+=30
         if s.find('给我搞')!=-1:
-            print("+++监测到有人正在赛艇")
+            print("++++监测到有人正在赛艇")
             ct+=50
-        print("此吧被感染程度（估）："+ct.__str__())
+        print("O----此吧被感染程度（估）："+ct.__str__())
         print("")
         if(ct>0):
             c.execute("INSERT INTO infected VALUES(\'"+parse.unquote(name)+"\',"+ct.__str__()+",CURRENT_TIMESTAMP)")
             conn.commit()
     else:
-        print("此吧已经被续\n")
+        print("O---此吧已经被续\n")
         c.execute("INSERT INTO blocked VALUES(\'"+parse.unquote(name)+"\',CURRENT_TIMESTAMP)")
         conn.commit()
     conn.close()
