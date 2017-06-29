51301537d392a037f6137a23fb0a9852835d55e7
NaiveWang <wanhchiwen@yahoo.com>
2017-06-29 15:48:29 +0800

    my issue fixed
    顺序由日期递增改为日期递减

--- export.py
+++ export.py
@@ -3,10 +3,10 @@ import sqlite3
 conn = sqlite3.connect('tieba.db3')
 c=conn.cursor()
 o=open('blocked.txt','w')
-for row in c.execute('SELECT * FROM blocked'):
+for row in c.execute('SELECT * FROM blocked ORDER BY timestamp DESC'):
     o.writelines(row[0]+'\n')
 o.close()
 o=open('infected.txt','w')
-for row in c.execute('SELECT * FROM infected'):
+for row in c.execute('SELECT * FROM infected ORDER BY timestamp DESC'):
     o.writelines(row[0]+'\n')
 o.close()
